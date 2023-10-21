cea83a7c4fd16d00df815d5fc31d6150fc3dced4
Alfredo de Moura Neto <moura03@yahoo.com.br>
2023-10-21 20:02:13 -0300

    Add files via upload

---
+++ meu_site.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 21 18:54:58 2023
+
+@author: moura
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return render_template("usuarios.html", nome_usuario=nome_usuario)
+
+if __name__ == "__main__":
+    app.run(debug=True) 
