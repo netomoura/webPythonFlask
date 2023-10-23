bffcce01c8e3521a8f22b2b4ad3dde2c128d4420
Alfredo de Moura Neto <moura03@yahoo.com.br>
2023-10-23 08:10:23 -0300

    Outro commit

--- meu_site.py
+++ meu_site.py
@@ -22,4 +22,5 @@ def usuarios(nome_usuario):
     return render_template("usuarios.html", nome_usuario=nome_usuario)
 
 #if __name__ == "__main__":
-    app.run(debug=True) 
+#   app.run(debug=True) 
+    app.run() 
